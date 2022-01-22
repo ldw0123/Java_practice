4842d67aea37ef1c814cfe36c39d20631a12e106
ldw0123 <ldw0123@gmail.com>
2022-01-23 04:37:26 +0900

    22.01.23.

---
+++ Chapter03/FloatDoubleEx.java
@@ -0,0 +1,16 @@
+package Chapter03;
+
+public class FloatDoubleEx {
+
+	public static void main(String[] args) {
+		float f = 3.141592653589793f;
+		double d = 3.141592653589793d;
+		
+		System.out.println("float: "+f);
+		System.out.println("double: "+d);
+		
+		double d2 = 3.141592653589793222; 	// d 생략가능
+		System.out.println("double: "+d2);
+	}
+
+}
