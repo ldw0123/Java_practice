6ebdbcbcf6153ac207bd8fe11ed884bc33ee240e
ldw0123 <ldw0123@gmail.com>
2022-01-26 18:13:27 +0900

    220126

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/9a/20f1deb7fa7d001c18a5c6c53d56f0fc
@@ -0,0 +1,9 @@
+package p220124;
+
+public class Day03_13 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- Day_03/src/p220124/Day03_12.java
+++ Day_03/src/p220124/Day03_12.java
@@ -1,8 +1,14 @@
+// p.64
+
 package p220124;
 
 public class Day03_12 {
 
 	public static void main(String[] args) {
+		int a = 10;
+		int b = 0;
+		
+		System.out.println(b>0 && (a/b >0)); 	// & ¿¬»ê
 		
 	}
 
